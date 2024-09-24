6e450bd2f4fab528b8fa4bfaa558e8431a65f066
Vera <78332987+vera2005@users.noreply.github.com>
2024-09-24 19:15:40 +0300

    Update main.go

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,40 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func removeDuplicates(inputStream chan string, outputStream chan string) {
+	prev := ""
+	for v := range inputStream {
+		if prev == "" || v != prev {
+			outputStream <- v
+		}
+		prev = v
+	}
+	close(outputStream)
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	inputStream := make(chan string, 5)
+	outputStream := make(chan string, 5)
+	//для теста создаем каналы с типом строка
+	// c размером буфера 5
+	s := bufio.NewScanner(os.Stdin)
+	//заполнение канала через ввод с клавиатуры
+	fmt.Printf("Put %d strings\n", cap(inputStream))
+	for i := 0; i < cap(inputStream); i++ {
+		s.Scan()
+		a := s.Text()
+		inputStream <- a
+	}
+	close(inputStream) // закрываем канал ввода
+	removeDuplicates(inputStream, outputStream)
+	fmt.Printf("In outputStream: ")
+	for v := range outputStream {
+		fmt.Print(v, "; ")
+	}
+
 }
